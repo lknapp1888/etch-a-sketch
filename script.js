143acd8655cc9efaca1b250929919ac917a8d091
Lewis Knapp <lknapp1888@gmail.com>
2022-10-05 00:01:41 +0100

    add working color input elements

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const gridContainer = document.getElementById('grid-container')
-const btn = document.getElementById('grid-btn')
+const clearGridbtn = document.getElementById('clear-grid-btn')
 const slider = document.getElementById('gridSize')
 const sliderNumber = document.getElementById('grid-range-number')
+const colorSelector = document.getElementById('color-selector')
+const borderColorSelector = document.getElementById('border-color-selector')
 const DEFAULT_GRID_SIZE = 16;
 
 
 addEventListener('load', setGridSize(16));
+
+clearGridbtn.addEventListener('click', clearGrid);
+
+function setBorderColor() {
+        document.querySelectorAll('.grid-item').forEach(element => {
+            element.style.borderColor = borderColorSelector.value;
+        });
+}
+
+borderColorSelector.oninput = setBorderColor;
 
 function setGridSize(num) {
     let gridNum = num;
@@ -25,24 +37,31 @@ function createGridDivs(num) {
         div.setAttribute('class', 'grid-item');
         div.classList.add(`grid-item-${i}`);
         div.addEventListener('mouseover', e => {
-            div.style.backgroundColor = 'black';
-            console.log(e)
+            div.style.backgroundColor = `${colorSelector.value}`;
+            div.style.borderColor = `${borderColorSelector.value}`;
         }) 
     }
 }
+ 
 
+
+
+//removes all grid square divs completely. NOT used to clear grid of colors
 function gridReset() {
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.firstChild);
       }
 }
 
-
-
+function clearGrid() {
+    setGridSize(slider.value);
+    setBorderColor();
+}
 
 slider.oninput = function() {
     sliderNumber.innerText = `${this.value}x${this.value}`;
-    setGridSize(this.value)
+    setGridSize(this.value);
+    setBorderColor();
   }
 
 
